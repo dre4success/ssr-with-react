68929b1236c33097252933519078d9486662a9a4
Damilare <dre4success@live.com>
2017-10-20 12:05:44 +0100

    using react-router-config for routing

--- server/src/client/components/UsersList.js
+++ server/src/client/components/UsersList.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {fetchUsers} from '../actions';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
 
 class UsersList extends Component {
   componentDidMount() {
-
+    this.props.fetchUsers();
   }
 
   renderUsers() {
     return this.props.users.map(user => {
-      return <li key={user.id}>{user.name}</li>
-    })
+      return <li key={user.id}>{user.name}</li>;
+    });
   }
-  render() { 
+  render() {
     return (
-      <div>Here's a big list of users</div>
+      <div>
+        Here's a big list of users
+        {this.renderUsers()}
+      </div>
     );
   }
 }
-function mapStateToProps({users}) {
-  return {users}
+function mapStateToProps({ users }) {
+  return { users };
 }
- 
-export default connect(mapStateToProps, {fetchUsers})(UsersList);
+
+export default connect(mapStateToProps, { fetchUsers })(UsersList);
--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
@@ -6,10 +7,10 @@ const app = express();
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-  const store = createStore()
+  const store = createStore();
 
   // some logic to initialize and store data
-  
+
   res.send(renderer(req, store));
 });
 
