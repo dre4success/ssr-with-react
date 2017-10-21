4c6accfd4591b8e58e14327f14c9f40e937b2258
Damilare <dre4success@live.com>
2017-10-21 21:26:10 +0100

    rendering a not found page

--- server/src/client/pages/HomePage.js
+++ server/src/client/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="center-margin" style={{marginTop: '200px'}}>
+    <div className="center-align" style={{marginTop: '200px'}}>
       <h3>Welcome</h3>
       <p>Check out these awesome features</p>
     </div>
